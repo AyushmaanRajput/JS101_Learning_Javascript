5857ea868dfe83b1801687b5fca417d2239d7ca8
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2023-04-11 10:21:57 +0530

    Changes made

--- Logical-Operators/problem2.js
+++
@@ -1,7 +0,0 @@
-let ch="z";
-
-if(ch=='a'|| ch=="e" || ch=="i" ||ch=="o"|| ch=="u"){
-  console.log("Vowel");
-}else{
-  console.log("Not a vowel");
-}
--- Logical-Operators/problem3.js
+++
@@ -1,7 +0,0 @@
-let ch="z";
-
-if(!(ch=='a') && !(ch=="e")  && !(ch=="i")  && !(ch=="o") && !(ch=="u")){
-  console.log("Consonant");
-}else {
-  console.log('Not A Consonant');
-}
