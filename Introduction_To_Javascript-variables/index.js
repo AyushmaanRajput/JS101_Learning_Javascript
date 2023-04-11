92b625f3ff31197e931845e2bfc7ece9a8369207
AyushmaanRajput <ayushmaanrajput25feb@gmail.com>
2023-04-11 10:12:33 +0530

    Introduction To Javascript-varibales

---
+++ Introduction_To_Javascript-variables/index.js
@@ -0,0 +1,5 @@
+let a = "Masai School";
+let b = "A Transformation In Education";
+
+console.log(a);
+console.log(b);
---
+++ Introduction_To_Javascript-variables/problem2.js
@@ -0,0 +1,6 @@
+let name = "Ayushmaan Rajput";
+consol.log(name);
+name = "Rajendra Rajput";
+console.log(name);
+name = "Veena Rajput";
+console.log(name);
